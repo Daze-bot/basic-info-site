95910ac88d8accb0adbc97b6736c5dea6f07acec
jevon-laptop <jevon.fillette@gmail.com>
2023-09-12 22:31:55 -0400

    fix bug to show server in console

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ const server = http.createServer((req, res) => {
   })
 });
 
-server.listen(port, hostname), () => {
+server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-};
+});
