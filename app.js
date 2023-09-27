0922370fe3db07f56400b19b996d0976b7280de1
jevon-laptop <jevon.fillette@gmail.com>
2023-09-26 21:12:09 -0400

    change to express

--- app.js
+++ app.js
@@ -2,25 +2,22 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + '/about.html');
+});
 
+app.get("/contact", (req, res) => {
+  res.sendFile(__dirname + '/contact.html');
+});
 
-
-
-
-
-
-
-
-
-
-
-
+app.use((req, res, next) => {
+  res.status(404).sendFile(__dirname + '/404.html');
+});
 
 app.listen(port, () => {
   console.log(`App listening on localhost:${port}`);
 });
-
-app.use((req, res, next) => {
-  res.status(404).sendFile('404.html');
-})
